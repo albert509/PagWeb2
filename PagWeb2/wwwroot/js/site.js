244f329467ab90505e19105613ef57f3c3756928
albert almonte <almonteasecas@hotmail.com>
2018-07-01 16:40:54 -0400

    Add project files.

---
+++ PagWeb2/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Write your JavaScript code.
+
+//sticky navbar
+window.onscroll = function () { myFunction() };
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("fixed-top")
+    } else {
+        navbar.classList.remove("fixed-top");
+    }
+} 
